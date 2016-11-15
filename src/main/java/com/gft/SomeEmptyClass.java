c64e3afe4e0b1a82af6ba8d7ffc33248db3f4b3e
blsmaciag <bartosz.maciag@gft.com>
2016-11-15 12:11:05 +0100

    pom version changed, new field in SomeEmptyClass

--- src/main/java/com/gft/SomeEmptyClass.java
+++ src/main/java/com/gft/SomeEmptyClass.java
@@ -1,4 +1,14 @@
 package com.gft;
 
 public class SomeEmptyClass {
+	private int someNumber;
+
+	public int getSomeNumber() {
+		return someNumber;
+	}
+
+	public void setSomeNumber() {
+		this.someNumber = someNumber;
+	}
+
 }
