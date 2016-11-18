bcece826fb8562c7220259922f6ddae23478a578
blsmaciag <bartosz.maciag@gft.com>
2016-11-18 09:33:55 +0100

    prapred changes to test cherry pick

---
+++ src/main/java/com/gft/model/Employee.java
@@ -0,0 +1,13 @@
+package com.gft.model
+
+public class Employee {
+	private String name;
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name){
+		this.name = name;
+	}
+}
